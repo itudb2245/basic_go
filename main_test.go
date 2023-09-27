f84df75c5cb9442955924edddf09dce834a2d44a
cetina19 <cetina19@itu.edu.tr>
2023-09-27 16:08:34 +0300

    pipeline

--- main_test.go
+++ main_test.go
@@ -4,14 +4,10 @@ import (
 	"testing"
 )
 
-func main(t *testing.T) {
+func TestSum(t *testing.T) {
 	actual := Sum(1, 2)
 	expected := 3
 	if actual != expected {
 		t.Errorf("Sum(1, 2): actual %v, expected %v", actual, expected)
 	}
-}
-
-func Sum(a int, b int) int {
-	return a + a
 }
