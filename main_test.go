786b8ac2aa82202ce7981520c46061fc2a193be4
cetina19 <cetina19@itu.edu.tr>
2023-09-27 15:57:20 +0300

    pipeline

--- main_test.go
+++ main_test.go
@@ -1,5 +1,11 @@
+package testing
+
+import (
+	"main"
+)
+
 func TestSum(t *testing.T) {
-	actual := Sum(1, 2)
+	actual := main.Sum(1, 2)
 	expected := 3
 	if actual != expected {
 		t.Errorf("Sum(1, 2): actual %v, expected %v", actual, expected)
